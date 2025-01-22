6b3bc5162e60d319d338b15db095f96c0a60d960
HoangThiThaoNhi <119788935+HoangThiThaoNhi@users.noreply.github.com>
2025-01-22 12:00:00 +0700

    Update source code

---
+++ thank_you.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thank You!</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="sytle.css">
+
+</head>
+
+<body>
+    <?php include_once 'header.php';
+    include_once 'contact_button.php'; ?>
+    <section class="thanks">
+        <div class="container py-5">
+            <div class="text-center">
+                <h1 class="tks-tiltle1">Thank You!</h1>
+                <p class="tks-tiltle2">Chúng tôi sẽ sớm gửi hàng đến bạn. Cảm ơn bạn đã tin tưởng và mua sắm tại cửa hàng của chúng tôi.</p>
+                <p class="tks-tiltle3">Tiếp tục mua sắm tại
+                    <a href="index.php">Phúc Long</a>
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <?php include_once 'footer.php'; ?>
+</body>
+
+</html>
