7b238d967e8a85f1b148cd99ebf9f59e5047a5b8
HoangThiThaoNhi <119788935+HoangThiThaoNhi@users.noreply.github.com>
2025-02-17 16:59:24 +0700

    Xóa các file không cần thiết

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); // Xóa tất cả các biến phiên
-session_destroy(); // Hủy phiên làm việc
-
-header('Location: login.php'); // Chuyển hướng đến trang đăng nhập
-exit();
-?>
